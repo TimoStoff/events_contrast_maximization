04ca35c3c98b7d81b5f5934f612d4721a31b1e6a
Timo Stoffregen <timo.stoffregen@outlook.com>
2020-06-08 15:56:31 +1000

    Modified h65 search

--- utils/event_utils.py
+++ utils/event_utils.py
@@ -7,13 +7,12 @@ import cv2 as cv
 import torch
 import torch.nn.functional as F
 
-def binary_search_h5_timestamp(hdf_path, l, r, x, side='left'):
-    f = h5py.File(hdf_path, 'r')
-    if r is None:
-        r = f.attrs['num_events']-1
+def binary_search_h5_dset(dset, x, l=None, r=None, side='left'):
+    l = 0 if l is None else l
+    r = len(dset)-1 if r is None else r
     while l <= r:
         mid = l + (r - l)//2;
-        midval = f['events/ts'][mid]
+        midval = dset[mid]
         if midval == x:
             return mid
         elif midval < x:
@@ -23,6 +22,10 @@ def binary_search_h5_timestamp(hdf_path, l, r, x, side='left'):
     if side == 'left':
         return l
     return r
+
+def binary_search_h5_timestamp(hdf_path, l, r, x, side='left'):
+    f = h5py.File(hdf_path, 'r')
+    return binary_search_h5_dset(f['events/ts'], x, l=l, r=r, side=side)
 
 def binary_search_torch_tensor(t, l, r, x, side='left'):
     if r is None:
